54d0ff1ccc2a5e41c0153b0c012c6dd1a17d7ccf
Chris Lee <cleer@cmu.edu>
2012-07-31 15:03:55 -0700

    Remove debugging line

--- data/script.js
+++ data/script.js
@@ -148,7 +148,6 @@ function hoverInfo() {
     $info.find('.visits').text(bookmark.visits);
     $info.find('.tags').empty();
     bookmark.tags.forEach(function(tag) {
-      console.log(tag);
       var html = '<li>' + tag + '</li>';
       var el = $(html);
       el.appendTo($info.find('.tags'));
