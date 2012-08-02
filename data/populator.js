d800128cb7ae856eb371d0a3df2c7f51a5dc8ab3
Chris Lee <cleer@cmu.edu>
2012-08-01 19:39:52 -0700

    Glyphicons instead of Symbolset. Closes #15.

--- data/populator.js
+++ data/populator.js
@@ -5,7 +5,7 @@ self.port.on('folder', function(folder) {
 });
 
 self.port.on('bookmark', function(bookmark) {
-  var html = '<li><a href="' + bookmark.location + '" target="_blank" style="background-image: url(' + bookmark.thumb + ')"><span class="icon"></span><span class="title">' + bookmark.title + '</span><span class="url">' + bookmark.location + '</span><span class="edit ss-icon">Pencil</span></a></li>'
+  var html = '<li><a href="' + bookmark.location + '" target="_blank" style="background-image: url(' + bookmark.thumb + ')"><span class="icon"></span><span class="title">' + bookmark.title + '</span><span class="url">' + bookmark.location + '</span><span class="edit"><span class="ui-icon">Edit</span></span></a></li>'
   var el = $(html);
   if (bookmark.icon) {
     el.find('.icon').css({ backgroundImage: "url(" + bookmark.icon + ")" });
