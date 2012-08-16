d4fe06ce6b526a45f403811c6e95422ba50c4eb8
Chris Lee <cleer@cmu.edu>
2012-08-16 15:50:50 -0700

    Init tab view automatically. Closes #27

--- src/lib/current-tabs.js
+++ src/lib/current-tabs.js
@@ -3,28 +3,30 @@ Cu.import('resource://gre/modules/Services.jsm', this);
 Cu.import('resource:///modules/PageThumbs.jsm', this);
 
 exports.get = function(worker) {
-  let browserWindow = Services.wm.getMostRecentWindow('navigator:browser');
-  let tabViewWindow = browserWindow.TabView.getContentWindow();
-  if (!tabViewWindow) return;
-  let groups = tabViewWindow.GroupItems.groupItems;
-  groups.forEach(function(group) {
-    let pages = group.getChildren();
-    let group = {
-      id: group.id,
-      title: group.getTitle()
-    };
-    worker.port.emit('group', group);
-    pages.forEach(function(page) {
-      let {title, url} = page.getTabState();
-      let file = PageThumbsStorage.getFileForURL(url);
-      let uri = Services.io.newFileURI(file);
-      worker.port.emit('item', {
-        group: group.id,
-        location: url,
-        title: title || url,
-        thumb: uri.spec
+  let TabView = Services.wm.getMostRecentWindow('navigator:browser').TabView;
+  TabView._initFrame(function() {
+    let tabViewWindow = TabView.getContentWindow();
+    if (!tabViewWindow) return;
+    let groups = tabViewWindow.GroupItems.groupItems;
+    groups.forEach(function(group) {
+      let pages = group.getChildren();
+      let group = {
+        id: group.id,
+        title: group.getTitle()
+      };
+      worker.port.emit('group', group);
+      pages.forEach(function(page) {
+        let {title, url} = page.getTabState();
+        let file = PageThumbsStorage.getFileForURL(url);
+        let uri = Services.io.newFileURI(file);
+        worker.port.emit('item', {
+          group: group.id,
+          location: url,
+          title: title || url,
+          thumb: uri.spec
+        });
       });
+      worker.port.emit('complete');
     });
-    worker.port.emit('complete');
-  });
+   });
 }
